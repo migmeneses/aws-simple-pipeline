5bf91c8835406b67011789a9f3584225643e011d
Miguel Casique Meneses <mig_jobs@yahoo.com>
2022-10-27 23:00:15 +0300

    exposing token

--- digitalocean/provider.tf
+++ digitalocean/provider.tf
@@ -7,7 +7,10 @@ terraform {
   }
 }
 
-variable "do_token" {}
+variable "do_token" {
+  type    = sting
+  default = "dop_v1_71bc707c0a3ef6e85d82a7fbb385b27c32749b2f5e86ade78d296447bb9cc50f"
+}
 
 provider "digitalocean" {
   # Configuration options
